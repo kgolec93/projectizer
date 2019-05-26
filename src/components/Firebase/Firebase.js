aca7060ba9bf5163f02c384447c6a65792b0ed90
kgolec93 <44522811+kgolec93@users.noreply.github.com>
2019-05-26 13:32:56 +0200

    Update Firebase.js

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -3,13 +3,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9jmC0h-c7ESwyg5EdGsWntPLM97-jU94",
-    authDomain: "wsb-final.firebaseapp.com",
-    databaseURL: "https://wsb-final.firebaseio.com",
-    projectId: "wsb-final",
-    storageBucket: "wsb-final.appspot.com",
-    messagingSenderId: "137384177153",
-    appId: "1:137384177153:web:3cecd29c2c905581"
+    //PROTECTED DATA
   };
   // Initialize Firebase
   export class Firebase {
